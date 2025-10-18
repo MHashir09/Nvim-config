5fddc33a8830bddd49eb09b6c3a30e53065213dd
MHashir09 <hashirzubair5742@gmail.com>
2025-10-18 23:25:26 +0500

    feat: disable hints etc because they were annoying me

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -27,19 +27,19 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            capabilities.textDocument.completion.completionItem.snippetSupport = true
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
 			local lspconfig = require("lspconfig")
-			lspconfig.html.setup({capabilities = capabilities,})
-			lspconfig.cssls.setup({capabilities = capabilities,})
-			lspconfig.tailwindcss.setup({capabilities = capabilities,})
-			lspconfig.emmet_ls.setup({capabilities = capabilities,})
-			lspconfig.ts_ls.setup({capabilities = capabilities,})
-			lspconfig.yamlls.setup({capabilities = capabilities,})
-			lspconfig.bashls.setup({capabilities = capabilities,})
-			lspconfig.lua_ls.setup({capabilities = capabilities,})
-			lspconfig.pyright.setup({capabilities = capabilities,})
-			lspconfig.clangd.setup({capabilities = capabilities,})
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.tailwindcss.setup({ capabilities = capabilities })
+			lspconfig.emmet_ls.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.yamlls.setup({ capabilities = capabilities })
+			lspconfig.bashls.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.pyright.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
 
 			vim.keymap.set("n", "I", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
@@ -50,8 +50,12 @@ return {
 				callback = function()
 					vim.diagnostic.config({
 						update_in_insert = false, -- turn it to truw if you want to see diagnostics in insert mode
-						virtual_text = true,
-						signs = true,
+						virtual_text = {
+							severity = { min = vim.diagnostic.severity.WARN }, -- this and signs below hide hints and stuff because it was realy annoying me. If you want that feature then comment out both severity objects and replace them with "true"
+						},
+						signs = {
+							severity = { min = vim.diagnostic.severity.WARN },
+						},
 						underline = false,
 						float = { border = "rounded" },
 					})
