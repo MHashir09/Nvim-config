8d9a2577556e575009ec8bf5d0b873057d2d78dc
MHashir09 <hashirzubair5742@gmail.com>
2025-10-19 02:35:49 +0500

    feat: add vim-binds for copy and pasting aswell as selecting whole files, add better visual aesthetics for line-numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,18 +10,27 @@ vim.cmd("let $PATH = '/home/hashir/.config/nvm/versions/node/v22.18.0/bin:' . $P
 -- in your terminal after installing node and npm.
 
 -- Line numbers
--- Enable line numbers
-vim.opt.number = true
+vim.opt.number = true          -- must be on for statuscolumn to render
 vim.opt.relativenumber = false
+vim.opt.numberwidth = 6        -- controls left padding width
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 
--- Make pink stick after colorscheme loads
+-- aesthetics for line-numbers
+local function apply_number_highlights()
+  pcall(vim.cmd, "highlight! link LineNr Title")
+  pcall(vim.cmd, "highlight! link CursorLineNr Title")
+end
+
+-- left padding: 2 spaces | right padding: 2 spaces
+vim.opt.statuscolumn = "%#LineNr#  %{v:lnum}  "
+
+apply_number_highlights()
+
 vim.api.nvim_create_autocmd("ColorScheme", {
   callback = function()
-    vim.cmd [[highlight LineNr guifg=pink]]
-    vim.cmd [[highlight CursorLineNr guifg=pink gui=bold]]
-  end
+    apply_number_highlights()
+  end,
 })
 
 -- Remove ~ from empty lines
@@ -36,5 +45,11 @@ vim.cmd [[autocmd BufWritePre * %s/\s\+$//e]]
 
 vim.g.lazy_git_timeout = 600
 
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true }) -- I did this because I like to use "jk" to enter normal mode rather than esc
 
+vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true, desc = "Select all text in file" }) -- this makes it easier to select all the text on the match
+
+vim.keymap.set("n", "yc", '"+yy', { noremap = true, silent = true }) -- to copy text
+vim.keymap.set("v", "yc", '"+y', { noremap = true, silent = true })  -- to copt text
+vim.keymap.set("n", "yp", '"+p', { noremap = true, silent = true })  -- to paste text
+vim.keymap.set("v", "yp", '"+p', { noremap = true, silent = true }) -- to paste text
