8794fca562135c8f50f1acf5099cf0c506d0e2c1
MHashir09 <hashirzubair5742@gmail.com>
2025-10-19 14:54:23 +0500

    feat: change the dashboard to snacks.nvim dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,91 +1,15 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = function()
-    -- Helper function to close dashboard buffer properly before opening a new window/command
-    local function open_and_close_dashboard(cmd)
-      local bufnr = vim.api.nvim_get_current_buf()
-      -- Create a new empty buffer to switch away from dashboard
-      vim.cmd('enew')
-      -- Delete dashboard buffer forcefully
-      vim.api.nvim_buf_delete(bufnr, { force = true })
-      -- Run the command
-      vim.cmd(cmd)
-    end
-
-    require('dashboard').setup {
-      theme = 'hyper',
-      config = {
-        header = {
-          [[  _________ **                 **_________                                 ]],
-          [[ /   _____//  |______ _______  \__    ___/___   _____ ______  ** ** ______]],
-          [[ \_____  \\   **\**  \\_  __ \   |    |_/ ** \ /     \\**__ \|  |  \/  ___/]],
-          [[ /        \|  |  / ** \|  | \/   |    |\  **_/|  Y Y  \  |_> >  |  /\___ \ ]],
-          [[/_______  /|__| (____  /__|      |____| \___  >__|_|  /   **/|**__//____  >]],
-          [[        \/           \/                     \/      \/|__|              \/ ]],
-        },
-        center = {
-          {
-            icon = ' ',
-            desc = 'Recently opened files',
-            action = function()
-              open_and_close_dashboard('Telescope oldfiles')
-            end,
-            shortcut = 'SPC f r',
-          },
-          {
-            icon = ' ',
-            desc = 'Find File',
-            action = function()
-              open_and_close_dashboard('Telescope find_files')
-            end,
-            shortcut = 'SPC f f',
-          },
-          {
-            icon = ' ',
-            desc = 'Find Word',
-            action = function()
-              open_and_close_dashboard('Telescope live_grep')
-            end,
-            shortcut = 'SPC f w',
-          },
-          {
-            icon = ' ',
-            desc = 'Open config',
-            action = function()
-              open_and_close_dashboard('edit ~/.config/nvim/init.lua')
-            end,
-            shortcut = 'SPC f c',
-          },
-        },
-        footer = { 'Have a nice day!' },
-      },
-      hide = {
-        statusline = true,
-        tabline = true,
-        winbar = true,
-      },
-      preview = {
-        command = nil,
-        file_path = nil,
-        file_height = 0,
-        file_width = 0,
-      },
-    }
-
-    -- Fix highlight colors in pink (#ff66cc)
-    vim.cmd [[
-      highlight clear DashboardHeader
-      highlight clear DashboardCenter
-      highlight clear DashboardShortCut
-      highlight clear DashboardFooter
-      highlight DashboardHeader    guifg=#ff66cc guibg=NONE gui=bold
-      highlight DashboardCenter    guifg=#ff66cc guibg=NONE
-      highlight DashboardShortCut  guifg=#ff66cc guibg=NONE gui=bold
-      highlight DashboardFooter    guifg=#ff66cc guibg=NONE
-      highlight ExtraWhitespace guibg=NONE
-    ]]
-  end,
+	"folke/snacks.nvim",
+	---@type snacks.Config
+	opts = {
+		dashboard = {
+				sections = {
+					{ section = "header" },
+					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{ section = "startup" },
+			},
+		},
+	},
 }
-
