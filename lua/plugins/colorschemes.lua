39cab2d6694a662e4a97cf8d231badc9f6ead5cb
MHashir09 <mhia09@proton.me>
2026-08-08 12:36:46 +0500

    Feat: Add new colorscheme Luna

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,15 +3,24 @@
 -- // As an example, I have commented out catppuccin, you can uncomment and comment out koda // --
 
 return {
-    "oskarnurm/koda.nvim",
-    lazy = false,       -- load immediately on startup
-    priority = 1000,    -- load before all other plugins
-    config = function()
-        require("koda").setup({ transparent = true })
-        vim.cmd("colorscheme koda-dark")
-    end,
+    -- // Koda, uncomment and comment out others
+    -- "oskarnurm/koda.nvim",
+    -- lazy = false,       -- load immediately on startup
+    -- priority = 1000,    -- load before all other plugins
+    -- config = function()
+    --     require("koda").setup({ transparent = true })
+    --     vim.cmd("colorscheme koda-dark")
+    -- end,
 
-    -- // Catppuccin alternative, uncomment and comment out koda to use
+     "wtfox/luna.nvim",
+     lazy = false,
+     priority = 1000,
+     config = function ()
+         require("luna").setup({ transparent = true })
+         vim.cmd("colorscheme luna")
+     end
+
+    -- // Catppuccin, uncomment and comment out others
     -- {
     --     "catppuccin/nvim",
     --     name = "catppuccin",
